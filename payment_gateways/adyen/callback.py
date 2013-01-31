aa72a545b3d47b8b89b668c002db5a22995f7563
kuraime <sisekeom@gmail.com>
2013-01-31 13:05:34 +0100

    Database results updated when the gw sends the response

--- payment_gateways/adyen/callback.py
+++ payment_gateways/adyen/callback.py
@@ -1,52 +1,40 @@
-from py_adyen.adyen import Adyen
-
+from payment_gateways.services import change_order_status
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-
 from django.shortcuts import render
-
 import sys
+
 
 def success(request):
     return render(request, 'success.html', {})
 
+
 def pending(request):
     return render(request, 'pending.html', {})
+
 
 def error(request):
     return render(request, 'error.html', {})
 
+
 @csrf_exempt
 def callback(request):
-    
-    if request.method == 'GET':
-        print "COMPLETE THIS CODE. DEAL WITH ALL POSSIBLE STATES and redirecited accordingly!"
-        return success(request)
-    
-    if request.method == 'POST': 
-        print "POST"
-        sys.stdout.flush()
-        
-        data = request.POST.dict()
-        
-        print "DICT"
-        print data
-        sys.stdout.flush()
-    
-        a = Adyen(data)
-        
-        print "Client"
-        sys.stdout.flush()
 
-        print "Parse data structure and store resutls in database!"
-    
-        if a.is_valid():
+    if request.method == 'GET':
+        data = request.GET.dict()
+        if data['success']:
+            return success(request)
+        else:
+            return error(request)
+    elif request.method == 'POST':
+        data = request.POST.dict()
+        if data['success']:
             print "VALID"
             sys.stdout.flush()
+            change_order_status(data['merchantReference'], "VALIDATED")
             return HttpResponse("[accepted]", mimetype="text/plain")
         else:
             print "ERROR"
             sys.stdout.flush()
+            change_order_status(data['merchantReference'], "ERROR")
             return HttpResponse("[error]", mimetype="text/plain")
-    
-    
--- payment_gateways/services.py
+++ payment_gateways/services.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Affero General Public License along w
 If not, see http://www.gnu.org/licenses/.
 
 For those usages not covered by the GNU Affero General Public License please contact with::mac@tid.es
-""" 
+"""
 
 '''
 Created on 30/10/2012
@@ -31,11 +31,12 @@ from api_format import UserData
 import importlib
 import uuid
 
+
 def initial_payment_url(token):
 
     user_data = get_user_data(token)
 
-    gws = PaymentGateway.objects.filter(country = user_data.country)
+    gws = PaymentGateway.objects.filter(country=user_data.country)
 
     if len(gws) == 0:
         return "/error"
@@ -48,13 +49,14 @@ def initial_payment_url(token):
     url = charger.get_redirect_url(user_data)
 
     store_master_information(user_data, charger.get_order(), gw)
-    
+
     return url
 
+
 def process_recurrent_payment(order_data):
-    
+
     store_order(order_data)
-    
+
     tef_account = order_data.tef_account
     country     = order_data.country
 
@@ -65,32 +67,35 @@ def process_recurrent_payment(order_data):
 
     # If several, getting the first one
     master_info = master_infos[0]
-    
+
     gw = master_info.gateway
-    
+
     charger = dynamically_loading_charger(gw)
 
     charger.recurrent_payment(order_data, master_info)
-    
+
     return 1
 
+
 def store_master_information(user_data, recurrent_order_code, gateway):
-    master_info = MasterInformation (tef_account=user_data.tef_account, recurrent_order_code=recurrent_order_code, 
+    master_info = MasterInformation(tef_account=user_data.tef_account, recurrent_order_code=recurrent_order_code,
                                      gateway=gateway, email=user_data.email)
     master_info.save()
 
+
 def store_order(order_data):
     order = Order(total=order_data.total, currency=order_data.currency, country=order_data.country,
-                  tef_account=order_data.tef_account, statement=order_data.statement, 
+                  tef_account=order_data.tef_account, statement=order_data.statement,
                   order_code=order_data.order_code)
-    
+
     order.save()
+
 
 def dynamically_loading_charger(gw):
     charger_module = importlib.import_module(gw.module_name)
     charger = getattr(charger_module, gw.class_name)(gw)
-    
     return charger
+
 
 def get_user_data(token):
     acquired_data = AcquiredData.objects.get(token=token)
@@ -102,10 +107,11 @@ def get_user_data(token):
 
     return user_data
 
+
 def generate_form_url(user_data):
     token = compute_unique_id()
 
-    acquired_data = AcquiredData(tef_account=user_data.tef_account,email=user_data.email,
+    acquired_data = AcquiredData(tef_account=user_data.tef_account, email=user_data.email,
                                  city=user_data.city, address=user_data.address,
                                  postal_code=user_data.postal_code, country=user_data.country,
                                  token=token)
@@ -114,7 +120,13 @@ def generate_form_url(user_data):
 
     return "http://payment-enabler.herokuapp.com/acquire/form/" + token
 
+
 def compute_unique_id():
     uid = uuid.uuid4()
     return uid.hex[:10]
 
+
+def change_order_status(order_code, status):
+    order = Order.objects.get(order_code=order_code)
+    order.status = status
+    order.save()
